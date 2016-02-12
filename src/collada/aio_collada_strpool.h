d6fec36ed487c84f6cdd47efa1f7c5ffc698bd82
Recep Aslantas <m@recp.me>
2016-02-12 19:46:13 +0200

    collada: create a python script to generate string pool offsets
    * python script will help us to avoid manual calculating of string
    offsets incorrectly and make easier to create new strings, change
    orders…

---
+++ src/collada/aio_collada_strpool.h
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef __libassetio__collada_strpool__h_
+#define __libassetio__collada_strpool__h_
+
+#ifndef _AIO_DAE_STRPOOL_
+extern
+#endif
+const char _s_dae_pool[];
+
+#ifndef _s_dae
+#define _s_dae(x) (_s_dae_pool + x)
+#endif
+
+#define _s_dae_space _s_dae(0)
+#define _s_dae_collada _s_dae(2)
+#define _s_dae_asset _s_dae(10)
+#define _s_dae_contributor _s_dae(16)
+#define _s_dae_author _s_dae(28)
+#define _s_dae_author_email _s_dae(35)
+#define _s_dae_author_website _s_dae(48)
+#define _s_dae_authoring_tool _s_dae(63)
+#define _s_dae_comments _s_dae(78)
+#define _s_dae_copyright _s_dae(87)
+#define _s_dae_source_data _s_dae(97)
+#define _s_dae_created _s_dae(109)
+#define _s_dae_modified _s_dae(117)
+#define _s_dae_keywords _s_dae(126)
+#define _s_dae_revision _s_dae(135)
+#define _s_dae_subject _s_dae(144)
+#define _s_dae_title _s_dae(152)
+#define _s_dae_unit _s_dae(158)
+#define _s_dae_name _s_dae(163)
+#define _s_dae_meter _s_dae(168)
+#define _s_dae_up_axis _s_dae(174)
+#define _s_dae_x_up _s_dae(182)
+#define _s_dae_z_up _s_dae(187)
+#define _s_dae_y_up _s_dae(192)
+#define _s_dae_extra _s_dae(197)
+#define _s_dae_lib_cameras _s_dae(203)
+#define _s_dae_id _s_dae(219)
+#define _s_dae_camera _s_dae(222)
+#define _s_dae_optics _s_dae(229)
+#define _s_dae_technique _s_dae(236)
+#define _s_dae_techniquec _s_dae(246)
+#define _s_dae_profile _s_dae(263)
+#define _s_dae_xmlns _s_dae(271)
+#define _s_dae_perspective _s_dae(277)
+#define _s_dae_xfov _s_dae(289)
+#define _s_dae_yfov _s_dae(294)
+#define _s_dae_sid _s_dae(299)
+#define _s_dae_aspect_ratio _s_dae(303)
+#define _s_dae_znear _s_dae(316)
+#define _s_dae_zfar _s_dae(322)
+#define _s_dae_orthographic _s_dae(327)
+#define _s_dae_xmag _s_dae(340)
+#define _s_dae_ymag _s_dae(345)
+#define _s_dae_ambient _s_dae(350)
+#define _s_dae_color _s_dae(358)
+#define _s_dae_directional _s_dae(364)
+#define _s_dae_point _s_dae(376)
+#define _s_dae_constant_attenuation _s_dae(382)
+#define _s_dae_linear_attenuation _s_dae(403)
+#define _s_dae_quadratic_attenuation _s_dae(422)
+#define _s_dae_spot _s_dae(444)
+#define _s_dae_falloff_angle _s_dae(449)
+#define _s_dae_falloff_exponent _s_dae(463)
+#define _s_dae_imager _s_dae(480)
+#define _s_dae_light _s_dae(487)
+#define _s_dae_lib_lights _s_dae(493)
+#define _s_dae_lib_effects _s_dae(508)
+#define _s_dae_effect _s_dae(524)
+#define _s_dae_annotate _s_dae(531)
+#define _s_dae_newparam _s_dae(540)
+#define _s_dae_prfl_common _s_dae(549)
+#define _s_dae_prfl_glsl _s_dae(564)
+#define _s_dae_prfl_gles2 _s_dae(577)
+#define _s_dae_prfl_gles _s_dae(591)
+#define _s_dae_prfl_cg _s_dae(604)
+#define _s_dae_prfl_bridge _s_dae(615)
+#define _s_dae_string _s_dae(630)
+#define _s_dae_bool _s_dae(637)
+#define _s_dae_bool2 _s_dae(642)
+#define _s_dae_bool3 _s_dae(648)
+#define _s_dae_bool4 _s_dae(654)
+#define _s_dae_int _s_dae(660)
+#define _s_dae_int2 _s_dae(664)
+#define _s_dae_int3 _s_dae(669)
+#define _s_dae_int4 _s_dae(674)
+#define _s_dae_float _s_dae(679)
+#define _s_dae_float2 _s_dae(685)
+#define _s_dae_float3 _s_dae(692)
+#define _s_dae_float4 _s_dae(699)
+#define _s_dae_float2x2 _s_dae(706)
+#define _s_dae_float3x3 _s_dae(715)
+#define _s_dae_float4x4 _s_dae(724)
+
+#endif /* __libassetio__collada_strpool__h_ */
---
+++ src/collada/strpool.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+#
+# Copyright (c), Recep Aslantas.
+#
+# MIT License (MIT), http://opensource.org/licenses/MIT
+# Full license can be found in the LICENSE file
+#
+
+import json
+from collections import OrderedDict
+from inspect import getsourcefile
+from os.path import realpath
+from os.path import dirname
+
+destdir = dirname(realpath(__file__))
+
+fspoolJSON = open(destdir + "/aio_collada_strpool.json")
+spool = json.loads(fspoolJSON.read(), object_pairs_hook=OrderedDict)
+fspoolJSON.close()
+
+fspool_h = open(destdir + "/aio_collada_strpool.h", "wb");
+fspool_c = open(destdir + "/aio_collada_strpool.c", "wb");
+
+copyright = """\
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+"""
+
+fspool_h.write(copyright)
+fspool_c.write(copyright)
+
+fspool_h.write("""
+#ifndef __libassetio__collada_strpool__h_
+#define __libassetio__collada_strpool__h_
+
+#ifndef _AIO_DAE_STRPOOL_
+extern
+#endif
+const char _s_dae_pool[];
+
+#ifndef _s_dae
+#define _s_dae(x) (_s_dae_pool + x)
+#endif
+
+""")
+
+fspool_c.write("""
+#ifndef _AIO_DAE_STRPOOL_
+#define _AIO_DAE_STRPOOL_
+#endif
+
+#include "aio_collada_strpool.h"
+#include <string.h>
+
+const char _s_dae_pool[] =
+""")
+
+pos = 0
+for name, val in spool.iteritems():
+  fspool_c.write("\"")
+  fspool_c.write(val)
+  fspool_c.write("\\0\"\n")
+
+  fspool_h.write("#define _s_dae_")
+  fspool_h.write(name)
+  fspool_h.write(" _s_dae(")
+  fspool_h.write(str(pos))
+  fspool_h.write(")\n")
+
+  pos += len(val) + 1
+
+fspool_h.write("""
+#endif /* __libassetio__collada_strpool__h_ */
+""")
+
+fspool_c.write(""";
+
+#undef _AIO_DAE_STRPOOL_
+""")
+
+fspool_h.close()
+fspool_c.close()
