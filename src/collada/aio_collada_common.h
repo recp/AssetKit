46a5cce1a7d65c4cd77e97dbf25fadb53b4316dc
Recep Aslantas <m@recp.me>
2016-02-12 19:56:22 +0200

    collada: replace DOM parser with xmlTextReader interface part0

--- src/collada/aio_collada_common.h
+++ src/collada/aio_collada_common.h
@@ -8,6 +8,16 @@
 #ifndef __libassetio__aio_collada_common__h_
 #define __libassetio__aio_collada_common__h_
 
+#include "aio_collada_strpool.h"
+#include "aio_libxml.h"
+#include "aio_types.h"
+#include "aio_memory.h"
+#include "aio_utils.h"
+#include "aio_tree.h"
+
+#include <libxml/xmlreader.h>
+#include <string.h>
+
 typedef struct _xmlNode xmlNode;
 
 extern
