89f2bf257feb835a40a8d0db10fed828f184f64a
Recep Aslantas <m@recp.me>
2016-03-14 18:38:02 +0200

    collada: fix includes

--- src/collada/aio_collada_common.h
+++ src/collada/aio_collada_common.h
@@ -9,11 +9,11 @@
 #define __libassetio__aio_collada_common__h_
 
 #include "aio_collada_strpool.h"
-#include "aio_libxml.h"
-#include "aio_common.h"
-#include "aio_memory.h"
-#include "aio_utils.h"
-#include "aio_tree.h"
+#include "../aio_libxml.h"
+#include "../aio_common.h"
+#include "../aio_memory.h"
+#include "../aio_utils.h"
+#include "../aio_tree.h"
 
 #include <libxml/xmlreader.h>
 #include <string.h>
