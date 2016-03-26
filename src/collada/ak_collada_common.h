43b3d4f6f29870b678cd8ba9bb89b29f3ec435ab
Recep Aslantas <m@recp.me>
2016-03-26 23:44:35 +0200

    include

--- src/collada/ak_collada_common.h
+++ src/collada/ak_collada_common.h
@@ -8,12 +8,13 @@
 #ifndef __libassetkit__ak_collada_common__h_
 #define __libassetkit__ak_collada_common__h_
 
-#include "ak_collada_strpool.h"
+#include "../../include/assetkit.h"
 #include "../ak_libxml.h"
 #include "../ak_common.h"
-#include "../ak_memory.h"
+#include "../../include/ak-memory.h"
 #include "../ak_utils.h"
 #include "../ak_tree.h"
+#include "ak_collada_strpool.h"
 
 #include <libxml/xmlreader.h>
 #include <string.h>
