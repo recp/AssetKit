4d430def6f4c50a44f208207f5bf5c1bc19d339b
Recep Aslantas <m@recp.me>
2016-03-31 11:11:19 +0300

    add missing header

---
+++ src/collada/core/ak_collada_geometry.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef __libassetkit__ak_collada_geometry_h_
+#define __libassetkit__ak_collada_geometry_h_
+
+#include "../ak_collada_common.h"
+
+AkResult _assetkit_hide
+ak_dae_geometry(void * __restrict memParent,
+                xmlTextReaderPtr reader,
+                AkGeometry ** __restrict dest);
+
+#endif /* __libassetkit__ak_collada_geometry_h_ */
