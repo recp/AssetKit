79ce6a97047688a4748d079a3b4a0b896550c2a5
Recep Aslantas <info@recp.me>
2016-10-20 19:15:27 +0300

    coordsys: change doc coordsys

---
+++ src/coord_sys/ak_coord_doc.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#include "../ak_common.h"
+#include "../ak_memory_common.h"
+
+AK_EXPORT
+void
+ak_changeCoordSys(AkDoc * __restrict doc,
+                  AkCoordSys * newCoordSys) {
+  AkLibGeometry *libGeom;
+  AkGeometry    *geom;
+
+  libGeom = doc->lib.geometries;
+
+  while (libGeom) {
+    geom = libGeom->chld;
+
+    while (geom) {
+      ak_changeCoordSysGeom(geom, newCoordSys);
+      geom = geom->next;
+    }
+
+    libGeom = libGeom->next;
+  }
+}
