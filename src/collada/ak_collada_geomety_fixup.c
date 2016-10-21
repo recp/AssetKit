e505f034ccbfb1381d45d179c0d40eec74a67f35
Recep Aslantas <info@recp.me>
2016-10-21 17:03:06 +0300

    style

--- src/collada/ak_collada_geomety_fixup.c
+++ src/collada/ak_collada_geomety_fixup.c
@@ -14,112 +14,112 @@ AkResult
 ak_dae_meshFixupPrimitive(AkHeap          *heap,
                           AkDoc           *doc,
                           AkMeshPrimitive *primitive) {
-    AkUIntArray *indices;
-    AkUIntArray *newIndices;
-    AkInput     *verticeInput;
-    AkInput     *input;
-    int          inputCount;
-    uint32_t     vertOffset;
-    size_t       indicesCount;
-    size_t       i;
+  AkUIntArray *indices;
+  AkUIntArray *newIndices;
+  AkInput     *verticeInput;
+  AkInput     *input;
+  int          inputCount;
+  uint32_t     vertOffset;
+  size_t       indicesCount;
+  size_t       i;
 
-    verticeInput = NULL;
-    inputCount   = 0;
-    input        = primitive->input;
-    indices      = primitive->indices;
+  verticeInput = NULL;
+  inputCount   = 0;
+  input        = primitive->input;
+  indices      = primitive->indices;
 
-    /* stride count */
-    while (input) {
-      inputCount++;
-      if (input->base.semantic == AK_INPUT_SEMANTIC_VERTEX)
-        verticeInput = input;
+  /* stride count */
+  while (input) {
+    inputCount++;
+    if (input->base.semantic == AK_INPUT_SEMANTIC_VERTEX)
+      verticeInput = input;
 
+    input = (AkInput *)input->base.next;
+  }
+
+  if (!verticeInput)
+    return AK_ERR;
+
+  vertOffset   = verticeInput->offset;
+  indicesCount = indices->count / inputCount;
+
+  /* fix indices for other inputs */
+  input = primitive->input;
+  while (input) {
+    AkSource *source;
+
+    if (input->base.semantic == AK_INPUT_SEMANTIC_VERTEX) {
       input = (AkInput *)input->base.next;
+      continue;
     }
 
-    if (!verticeInput)
-      return AK_ERR;
+    source = ak_getObjectByUrl(doc, input->base.source);
 
-    vertOffset   = verticeInput->offset;
-    indicesCount = indices->count / inputCount;
+    /* TODO: INT, DOUBLE.. */
+    if (source->data->type == AK_SOURCE_ARRAY_TYPE_FLOAT) {
+      AkObject      *obj;
+      AkFloatArrayN *oldArray;
+      AkFloatArrayN *newArray;
+      AkFloat       *oldItems;
+      AkFloat       *newItems;
+      size_t         j;
 
-    /* fix indices for other inputs */
-    input = primitive->input;
-    while (input) {
-      AkSource *source;
-
-      if (input->base.semantic == AK_INPUT_SEMANTIC_VERTEX) {
-        input = (AkInput *)input->base.next;
-        continue;
-      }
-
-      source = ak_getObjectByUrl(doc, input->base.source);
-
-      /* TODO: INT, DOUBLE.. */
-      if (source->data->type == AK_SOURCE_ARRAY_TYPE_FLOAT) {
-        AkObject      *obj;
-        AkFloatArrayN *oldArray;
-        AkFloatArrayN *newArray;
-        AkFloat       *oldItems;
-        AkFloat       *newItems;
-        size_t         j;
-
-        /* TODO: replace 3 with vertex count */
+      /* TODO: replace 3 with vertex count */
 #define vertc 3
-        obj = ak_objAlloc(heap,
-                          source,
-                          sizeof(*newArray)
-                          + sizeof(AkFloat) * indicesCount * vertc,
-                          AK_SOURCE_ARRAY_TYPE_FLOAT,
-                          false,
-                          false);
+      obj = ak_objAlloc(heap,
+                        source,
+                        sizeof(*newArray)
+                        + sizeof(AkFloat) * indicesCount * vertc,
+                        AK_SOURCE_ARRAY_TYPE_FLOAT,
+                        false,
+                        false);
 
-        newArray = ak_objGet(obj);
-        oldArray = ak_objGet(source->data);
+      newArray = ak_objGet(obj);
+      oldArray = ak_objGet(source->data);
 
-        oldItems = oldArray->items;
-        newItems = newArray->items;
+      oldItems = oldArray->items;
+      newItems = newArray->items;
 
-        newArray->count     = indicesCount * vertc;
-        newArray->digits    = oldArray->digits;
-        newArray->magnitude = oldArray->magnitude;
+      newArray->count     = indicesCount * vertc;
+      newArray->digits    = oldArray->digits;
+      newArray->magnitude = oldArray->magnitude;
 
-        if (oldArray->name) {
-          newArray->name = oldArray->name;
-          ak_mem_setp((void *)oldArray->name, newArray);
-        }
-
-        /* fix indices */
-        for (i = 0; i < indicesCount; i++) {
-          AkUInt index;
-          AkUInt index2;
-
-          index  = indices->items[i * inputCount + vertOffset];
-          index2 = indices->items[i * inputCount + input->offset];
-
-          for (j = 0; j < vertc; j++)
-            newItems[index * vertc + j] = oldItems[index2 * vertc + j];
-        }
-
-        ak_free(source->data);
-        source->data = obj;
+      if (oldArray->name) {
+        newArray->name = oldArray->name;
+        ak_mem_setp((void *)oldArray->name, newArray);
       }
 
-      input = (AkInput *)input->base.next;
+      /* fix indices */
+      for (i = 0; i < indicesCount; i++) {
+        AkUInt index;
+        AkUInt index2;
+
+        index  = indices->items[i * inputCount + vertOffset];
+        index2 = indices->items[i * inputCount + input->offset];
+
+        for (j = 0; j < vertc; j++)
+          newItems[index * vertc + j] = oldItems[index2 * vertc + j];
+      }
+
+      ak_free(source->data);
+      source->data = obj;
     }
 
-    newIndices = ak_heap_alloc(heap,
-                               primitive,
-                               sizeof(AkFloat) * indicesCount,
-                               false);
+    input = (AkInput *)input->base.next;
+  }
 
-    newIndices->count = indicesCount;
+  newIndices = ak_heap_alloc(heap,
+                             primitive,
+                             sizeof(AkFloat) * indicesCount,
+                             false);
 
-    /* keep only vertex indices in primitives */
-    for (i = 0; i < indicesCount; i++)
-      newIndices->items[i] = indices->items[i * inputCount + vertOffset];
+  newIndices->count = indicesCount;
 
-    ak_free(indices);
+  /* keep only vertex indices in primitives */
+  for (i = 0; i < indicesCount; i++)
+    newIndices->items[i] = indices->items[i * inputCount + vertOffset];
+
+  ak_free(indices);
 
   primitive->indices = newIndices;
 
