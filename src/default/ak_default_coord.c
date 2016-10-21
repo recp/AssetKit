7372822d2dca623c2419266be392fed16837f900
Recep Aslantas <info@recp.me>
2016-10-21 18:11:30 +0300

    reduce object in stack
    * now ak_defaultCoordSys() == AK_YUP is true

--- src/default/ak_default_coord.c
+++ src/default/ak_default_coord.c
@@ -8,25 +8,27 @@
 #include "../ak_common.h"
 #include "../ak_memory_common.h"
 
-#define AK__Z_RH { AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Z, AK_AXIS_POSITIVE_Y }
-#define AK__Y_RH { AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Y, AK_AXIS_NEGATIVE_Z }
-#define AK__X_RH { AK_AXIS_NEGATIVE_Y, AK_AXIS_POSITIVE_X, AK_AXIS_NEGATIVE_Z }
+#define AK__Z_RH AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Z, AK_AXIS_POSITIVE_Y
+#define AK__Y_RH AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Y, AK_AXIS_NEGATIVE_Z
+#define AK__X_RH AK_AXIS_NEGATIVE_Y, AK_AXIS_POSITIVE_X, AK_AXIS_NEGATIVE_Z
 
-#define AK__Z_LH { AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Z, AK_AXIS_NEGATIVE_Y }
-#define AK__Y_LH { AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Y, AK_AXIS_POSITIVE_Z }
-#define AK__X_LH { AK_AXIS_NEGATIVE_Y, AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Z }
+#define AK__Z_LH AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Z, AK_AXIS_NEGATIVE_Y
+#define AK__Y_LH AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Y, AK_AXIS_POSITIVE_Z
+#define AK__X_LH AK_AXIS_NEGATIVE_Y, AK_AXIS_POSITIVE_X, AK_AXIS_POSITIVE_Z
+
+AkCoordSys AK__Y_RH_VAL = {AK__Y_RH};
 
 /* Right Hand (Default) */
-AkCoordSys * AK_ZUP = &(AkCoordSys)AK__Z_RH;
-AkCoordSys * AK_YUP = &(AkCoordSys)AK__Y_RH;
-AkCoordSys * AK_XUP = &(AkCoordSys)AK__X_RH;
+AkCoordSys * AK_YUP    = &AK__Y_RH_VAL;
+AkCoordSys * AK_ZUP    = &(AkCoordSys){AK__Z_RH};
+AkCoordSys * AK_XUP    = &(AkCoordSys){AK__X_RH};
 
 /* Left Hand */
-AkCoordSys * AK_ZUP_LH = &(AkCoordSys)AK__Z_LH;
-AkCoordSys * AK_YUP_LH = &(AkCoordSys)AK__Y_LH;
-AkCoordSys * AK_XUP_LH = &(AkCoordSys)AK__X_LH;
+AkCoordSys * AK_ZUP_LH = &(AkCoordSys){AK__Z_LH};
+AkCoordSys * AK_YUP_LH = &(AkCoordSys){AK__Y_LH};
+AkCoordSys * AK_XUP_LH = &(AkCoordSys){AK__X_LH};
 
-AkCoordSys * AK_DEFAULT_COORD = &(AkCoordSys)AK__Y_RH;
+AkCoordSys * AK_DEFAULT_COORD = &AK__Y_RH_VAL;
 
 AK_EXPORT
 void
