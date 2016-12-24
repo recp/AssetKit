9ca08a7c2f98148d3ddb694ecb2642a1e262233d
Recep Aslantas <info@recp.me>
2016-12-24 23:39:33 +0200

    rename options

--- include/ak-options.h
+++ include/ak-options.h
@@ -17,8 +17,8 @@ typedef enum AkOption {
   AK_OPT_INDICES_SINGLE_INTERLEAVED = 1,  /* false  */
   AK_OPT_INDICES_SINGLE_SEPARATE    = 2,  /* false  */
   AK_OPT_INDICES_SINGLE             = 3,  /* false  */
-  AK_OPT_NO_INDICES_INTERLEAVED     = 4,  /* true  */
-  AK_OPT_NO_INDICES_SEPARATE        = 5,  /* true  */
+  AK_OPT_NOINDEX_INTERLEAVED        = 4,  /* true  */
+  AK_OPT_NOINDEX_SEPARATE           = 5,  /* true  */
   AK_OPT_COORD                      = 6,  /* Y_UP  */
   AK_OPT_DEFAULT_ID_PREFIX          = 7   /* id prefix */
 } AkOption;
