4c1b3b3581c3b689f71d2efe7021e5520f885419
Recep Aslantas <info@recp.me>
2017-01-14 17:14:37 +0200

    remove bitwise.c and force all func inline

--- src/bitwise/ak_bitwise.h
+++ src/bitwise/ak_bitwise.h
@@ -10,13 +10,39 @@
 
 #include "../ak_common.h"
 
+AK_INLINE
 uint32_t
-ak_bitw_ctz(uint32_t x);
+ak_bitw_ctz(uint32_t x) {
+#if __has_builtin(__builtin_ctz)
+	return __builtin_ctz(x);
+#else
+	uint32_t i;
 
-uint32_t
-ak_bitw_clz(uint32_t x);
+	i = 0;
+	while ((x >>= 1) > 0)
+		i++;
+	return i;
+#endif
+}
 
+AK_INLINE
 uint32_t
-ak_bitw_ffs(uint32_t x);
+ak_bitw_ffs(uint32_t x) {
+#if __has_builtin(__builtin_ffs)
+	return __builtin_ffs(x);
+#else
+	return ak_bitw_ctz(x) + 1;
+#endif
+}
+
+AK_INLINE
+uint32_t
+ak_bitw_clz(uint32_t x) {
+#if __has_builtin(__builtin_clz)
+	return __builtin_clz(x);
+#else
+	return sizeof(uint32_t) * CHAR_BIT - ak_bitw_ffs(x);
+#endif
+}
 
 #endif /* ak_bitwise_h */
