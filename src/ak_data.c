3f4813c8d32ef3bbc98bd496f5c5e436b61595bf
Recep Aslantas <info@recp.me>
2017-02-05 22:15:51 +0300

    implement a container for creating unique arrays
    * this will be used for creating unique-items  arrays e.g. positions,
    normals...

---
+++ src/ak_data.c
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#include "ak_data.h"
+#include <assert.h>
+
+AkDataContext*
+ak_data_new(void   *memparent,
+            size_t  nodeitems,
+            size_t  itemsize,
+            AkCmpFn cmp) {
+  AkHeap        *heap;
+  AkDataContext *ctx;
+
+  heap = ak_heap_getheap(memparent);
+  ctx  = ak_heap_calloc(heap, memparent, sizeof(*ctx));
+
+  ctx->nodesize = nodeitems * itemsize;
+  ctx->itemsize = itemsize;
+  ctx->heap     = heap;
+  ctx->cmp      = cmp;
+
+  return ctx;
+}
+
+void
+ak_data_append(AkDataContext *dctx, void *data) {
+  AkDataChunk *chunk;
+  void        *mem;
+  size_t       size;
+
+  size = dctx->itemsize;
+  assert(dctx->nodesize > size);
+
+  if (dctx->usedsize + size > dctx->size) {
+    chunk = ak_heap_alloc(dctx->heap,
+                          dctx,
+                          sizeof(*chunk) + dctx->nodesize);
+    chunk->usedsize = 0;
+    chunk->next     = NULL;
+
+    chunk->next = dctx->last;
+    dctx->last  = chunk;
+    dctx->size += dctx->nodesize;
+
+    if (!dctx->data)
+      dctx->data = chunk;
+  } else {
+    chunk = dctx->last;
+  }
+
+  mem = chunk->data + chunk->usedsize;
+  memcpy(mem, data, size);
+
+  chunk->usedsize += size;
+  dctx->usedsize  += size;
+  dctx->itemcount++;
+}
+
+void
+ak_data_append_unq(AkDataContext *dctx, void *item) {
+  if (ak_data_exists(dctx, item))
+    return;
+
+  ak_data_append(dctx, item);
+}
+
+void
+ak_data_walk(AkDataContext *dctx) {
+  AkDataChunk *chunk;
+  void  *data;
+  size_t isz, csz, i;
+
+  if (!dctx->data)
+    return;
+
+  isz   = dctx->itemsize;
+  chunk = dctx->data;
+
+  while (chunk) {
+    csz = dctx->nodesize - chunk->usedsize;
+    for (i = isz; i < csz; i += isz) {
+      data = chunk->data;
+      dctx->walkFn(dctx, data);
+    }
+    chunk = chunk->next;
+  }
+}
+
+bool
+ak_data_exists(AkDataContext *dctx, void *item) {
+  AkDataChunk *chunk;
+  void  *data;
+  char  *pmem;
+  size_t isz, csz, i;
+
+  if (!dctx->data)
+    return false;
+
+  isz   = dctx->itemsize;
+  chunk = dctx->data;
+
+  while (chunk) {
+    csz  = dctx->nodesize - chunk->usedsize;
+    data = chunk->data;
+    pmem = data;
+
+    for (i = isz; i < csz; i += isz) {
+      if (dctx->cmp(pmem, item) == 0)
+        return true;
+
+      pmem += isz;
+    }
+    chunk = chunk->next;
+  }
+
+  return false;
+}
+
+void*
+ak_data_join(AkDataContext *dctx,
+             bool           freeit) {
+  AkDataChunk *chunk;
+  void  *mem, *data;
+  char  *pmem;
+  size_t isz, csz, i;
+
+  if (!dctx->data)
+    return NULL;
+
+  isz   = dctx->itemsize;
+  chunk = dctx->data;
+  mem   = ak_heap_alloc(dctx->heap,
+                        dctx->memparent,
+                        dctx->usedsize);
+
+  pmem = mem;
+  while (chunk) {
+    csz = dctx->nodesize - chunk->usedsize;
+    for (i = isz; i < csz; i += isz) {
+      data = chunk->data;
+      memcpy(pmem, data, isz);
+      pmem += isz;
+    }
+    chunk = chunk->next;
+  }
+
+  if (freeit)
+    ak_free(dctx);
+
+  return mem;
+}
---
+++ src/ak_data.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef ak_data_h
+#define ak_data_h
+
+#include "ak_common.h"
+
+struct AkDataContext;
+
+typedef void (*AkDataContextWalkFn)(struct AkDataContext *dctx,
+                                    void                 *item);
+
+typedef struct AkDataChunk {
+  struct AkDataChunk *next;
+  size_t              usedsize;
+  char                data[];
+} AkDataChunk;
+
+typedef struct AkDataContext {
+  void                *memparent;
+  AkHeap              *heap;
+  AkDataChunk         *data;
+  AkDataChunk         *last;
+  AkCmpFn              cmp;
+  AkDataContextWalkFn  walkFn;
+  size_t               nodesize;
+  size_t               size;
+  size_t               usedsize;
+  size_t               itemsize;
+  size_t               itemcount;
+} AkDataContext;
+
+AkDataContext*
+ak_data_new(void   *memparent,
+            size_t  nodeitems,
+            size_t  itemsize,
+            AkCmpFn cmp);
+
+/*!
+ * @brief join batch data into continued array
+ *
+ * @param dctx data context
+ * @param free free after joining
+ *
+ * @return continued memory adress
+ */
+void*
+ak_data_join(AkDataContext *dctx, bool freeit);
+
+void
+ak_data_append(AkDataContext *dctx, void *data);
+
+void
+ak_data_append_unq(AkDataContext *dctx, void *item);
+
+void
+ak_data_walk(AkDataContext *dctx);
+
+bool
+ak_data_exists(AkDataContext *dctx, void *item);
+
+#endif /* ak_data_h */
