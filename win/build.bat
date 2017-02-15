22ec7467ad781a191cf85fb5f62637ef3a240409
Recep Aslantas <info@recp.me>
2017-02-15 13:32:20 +0300

    build: windows

--- win/build.bat
+++ win/build.bat
@@ -7,7 +7,7 @@ git submodule -q update --init --recursive 2> $null
 
 cd lib\libuv
 git clone -q https://chromium.googlesource.com/external/gyp.git build/gyp 2> $null
-.\vcbuild.bat
+.\vcbuild.bat 2> $null
 
 cd %CUR_DR%
 
