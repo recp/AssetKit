8e6bdfaaa7f2679ce7656d2c68b993f868b8c529
Recep Aslantas <info@recp.me>
2017-07-25 10:21:55 +0300

    build, win: fix include headers and visual studio 2017 vars

--- win/build.bat
+++ win/build.bat
@@ -1,5 +1,10 @@
 @echo off
 
+set vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+for /f "usebackq tokens=1* delims=: " %%i in (`"%vswhere%" -latest -requires Microsoft.Component.MSBuild`) do (
+  if /i "%%i"=="installationPath" set InstallDir=%%j
+)
+
 SET CUR_DR=%cd%
 nuget restore .\assetkit.sln
 
@@ -8,19 +13,23 @@ cd ..
 git submodule -q update --init --recursive 2> $null
 
 echo.
-echo Build libuv
-echo.
-
-cd lib\libuv
-git clone -q https://chromium.googlesource.com/external/gyp.git build/gyp 2> $null
-call .\vcbuild.bat
-
-echo.
 echo Build libds
 echo.
 
 cd %CUR_DR%\..\lib\libds\win
 call .\build.bat
+
+echo.
+echo Build libuv
+echo.
+
+cd %CUR_DR%\..\lib\libuv
+git clone -q https://chromium.googlesource.com/external/gyp.git build/gyp 2> $null
+.\vcbuild.bat
+
+if exist "%InstallDir%\VC\Auxiliary\Build\vcvars64.bat" (
+  call "%InstallDir%\VC\Auxiliary\Build\vcvars64.bat"
+)
 
 cd %CUR_DR%
 
