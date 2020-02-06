e6fb1c59178bcf373886ead5eec865f4ac17bccb
Recep Aslantas <info@recp.me>
2020-02-06 20:41:36 +0300

    dae: fix strpool script for new file names

--- src/collada/strpool.py
+++ src/collada/strpool.py
@@ -16,13 +16,13 @@ destdir        = dirname(realpath(__file__))
 spidx          = 0
 pos            = 0
 
-fspoolJson = open(destdir + "/dae_strpool.json")
+fspoolJson = open(destdir + "/strpool.json")
 spool      = json.loads(fspoolJson.read(),
                         object_pairs_hook=OrderedDict)
 fspoolJson.close()
 
-fspool_h = open(destdir + "/dae_strpool.h", "wb");
-fspool_c = open(destdir + "/dae_strpool.c", "wb");
+fspool_h = open(destdir + "/strpool.h", "wb");
+fspool_c = open(destdir + "/strpool.c", "wb");
 
 copyright_str = """\
 /*
