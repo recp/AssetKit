805048a6503276618dab78b480249c4acb80dd10
Recep Aslantas <info@recp.me>
2020-04-05 23:40:30 +0300

    remove unused functions

--- include/ak/string.h
+++ include/ak/string.h
@@ -72,63 +72,6 @@ ak_strtob_fast(char    * __restrict src,
                AkBool  * __restrict dest);
 
 AK_EXPORT
-int
-ak_strtod(char ** __restrict src,
-          AkDouble * __restrict dest,
-          unsigned long n);
-
-AK_EXPORT
-int
-ak_strtof(char ** __restrict src,
-           AkFloat * __restrict dest,
-           unsigned long n);
-
-AK_EXPORT
-int
-ak_strtoui(char   ** __restrict src,
-           AkUInt * __restrict dest,
-           unsigned long n);
-
-AK_EXPORT
-int
-ak_strtomb(char ** __restrict src,
-            AkBool * __restrict dest,
-            unsigned long m,
-            unsigned long n);
-
-AK_EXPORT
-int
-ak_strtomi(char ** __restrict src,
-            AkInt * __restrict dest,
-            unsigned long m,
-            unsigned long n);
-
-AK_EXPORT
-int
-ak_strtomf(char ** __restrict src,
-            AkFloat * __restrict dest,
-            unsigned long m,
-            unsigned long n);
-
-AK_EXPORT
-int
-ak_strtof_s(const char * __restrict src,
-             AkFloat * __restrict dest,
-             unsigned long n);
-
-AK_EXPORT
-extern
-int
-ak_strtof4(char ** __restrict src,
-            AkFloat4 * __restrict dest);
-
-AK_EXPORT
-extern
-int
-ak_strtof4_s(const char * __restrict src,
-              AkFloat4 * __restrict dest);
-
-AK_EXPORT
 char*
 ak_tolower(char *str);
 
