787d07725efa6b7d7dd624f09b29abf0e8e08439
Recep Aslantas <info@recp.me>
2020-04-16 17:53:11 +0300

    win: update build.bat

--- win/build.bat
+++ win/build.bat
@@ -6,7 +6,6 @@ for /f "usebackq tokens=1* delims=: " %%i in (`"%vswhere%" -latest -requires Mic
 )
 
 SET CUR_DR=%cd%
-nuget restore .\assetkit.sln
 
 cd ..
 
@@ -18,18 +17,6 @@ echo.
 
 cd %CUR_DR%\..\lib\libds\win
 call .\build.bat
-
-echo.
-echo Build libuv
-echo.
-
-cd %CUR_DR%\..\lib\libuv
-git clone -q https://chromium.googlesource.com/external/gyp.git build/gyp 2> $null
-.\vcbuild.bat
-
-if exist "%InstallDir%\VC\Auxiliary\Build\vcvars64.bat" (
-  call "%InstallDir%\VC\Auxiliary\Build\vcvars64.bat"
-)
 
 cd %CUR_DR%
 
